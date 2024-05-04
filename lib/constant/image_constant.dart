36209a522f50f3b347b0c7fa31541cccc80ac33b
EqiBaihaqi <baihaqi08.aa@gmail.com>
2024-05-04 13:52:36 +0700

    Add image when null

--- lib/constant/image_constant.dart
+++ lib/constant/image_constant.dart
@@ -2,4 +2,5 @@ class ImageConstant {
   static String logo = 'assets/images/logo.png';
   static String logoBlack = 'assets/images/logo_black.png';
   static String landingPage = 'assets/images/landing_page2.png';
+  static String noImage = 'assets/images/no_image.jpg';
 }
