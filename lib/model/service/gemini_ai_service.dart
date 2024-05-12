d11199f55f0b8280e9dc3198a19dba3ef86207a9
EqiBaihaqi <baihaqi08.aa@gmail.com>
2024-05-13 01:17:01 +0700

    Edit Prompt Gemmini AI

--- lib/model/service/gemini_ai_service.dart
+++ lib/model/service/gemini_ai_service.dart
@@ -9,7 +9,7 @@ class GeminiService {
   static Future<String> getGeminiResponse(String? ingredient) async {
     try {
       final String question =
-          'Tolong berikan alternatif bahan makanan dari $ingredient, tolong berikan jawaban yang tidak bertele-tele dan langsung ke inti jawabannya dan jawab menggunakan bahasa inggris, tolong jangan gunakan ikon *. dan jika $ingredient bukanlah sebuah bahan makanan, maka jawab "maaf $ingredient bukanlah bahan makanan" dalam bahasa inggris ';
+          'Tolong berikan alternatif bahan masakan dari $ingredient, tolong berikan jawaban yang tidak bertele-tele dan langsung ke inti jawabannya dan jawab menggunakan bahasa inggris, tolong jangan gunakan ikon *. dan jika $ingredient bukanlah sebuah bahan masakan, maka jawab "maaf $ingredient bukanlah bahan masakan" dalam bahasa inggris ';
       Completer<String> completer = Completer<String>();
       gemini.streamGenerateContent(question).listen((event) {
         String response = event.content?.parts?.fold('',
