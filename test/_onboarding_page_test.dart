32593df44dbe94825ef89eda67892987017cc8eb
EqiBaihaqi <baihaqi08.aa@gmail.com>
2024-05-13 18:55:48 +0700

    Remove Unused

--- test/_onboarding_page_test.dart
+++ test/_onboarding_page_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:your_comfort_food/page/home.dart';
-import 'package:your_comfort_food/page/login_page/login_page.dart';
+
 import 'package:your_comfort_food/page/onboarding_page/widgets/screen_1.dart';
-import 'package:your_comfort_food/page/search_page/search_page.dart';
 
 void main() {
   testWidgets('Harus ada Text \'Includge in Culinary Delights!\'',
