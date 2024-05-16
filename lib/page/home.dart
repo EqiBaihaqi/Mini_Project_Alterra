f8192a6b75a890f18252daed0d81103b15374d1a
EqiBaihaqi <baihaqi08.aa@gmail.com>
2024-05-16 14:03:21 +0700

    Move Log out button

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
-import 'package:your_comfort_food/constant/color_constant.dart';
+
 import 'package:your_comfort_food/page/alternate_ingredient/alternate_ingredient_page.dart';
 import 'package:your_comfort_food/page/home_page/home_page.dart';
 import 'package:your_comfort_food/page/home_page/home_page_view_model.dart';
-import 'package:your_comfort_food/page/login_page/login_page.dart';
-import 'package:your_comfort_food/page/login_page/login_view_model.dart';
+
+
 import 'package:your_comfort_food/page/search_page/search_page.dart';
 import 'package:your_comfort_food/widgets/bottom_navigation_bar/bottom_navigation_bar_view_model.dart';
 import 'package:your_comfort_food/widgets/bottom_navigation_bar/bottom_navigation_bar_widget.dart';
@@ -36,7 +36,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    final providerLogin = Provider.of<LoginPageViewModel>(context);
+
     final navigationProvider = Provider.of<BottomNavigationProvider>(context);
     List<Widget> body = [
       const HomePage(),
@@ -44,48 +44,9 @@ class _HomeState extends State<Home> {
       const AlternateIngredientPage(),
     ];
     return Scaffold(
-        extendBody: true,
-        body: body[navigationProvider.currentIndex],
-        bottomNavigationBar: const BottomNavCustom(),
-        floatingActionButton: FloatingActionButton(
-          shape:
-              BeveledRectangleBorder(borderRadius: BorderRadius.circular(25)),
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: const Text('Log out'),
-                  content: const Text('You sure want to log out?'),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        providerLogin.logOut();
-                        Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const LoginPage()),
-                            (route) => false);
-                      },
-                      child: const Text('Log Out'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Cancel'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-          backgroundColor: ColorConstant.whitishGray,
-          child: Icon(
-            Icons.logout_outlined,
-            color: ColorConstant.orangeColor2,
-            size: 27,
-          ),
-        ));
+      extendBody: true,
+      body: body[navigationProvider.currentIndex],
+      bottomNavigationBar: const BottomNavCustom(),
+    );
   }
 }
--- lib/page/home_page/home_page.dart
+++ lib/page/home_page/home_page.dart
@@ -9,6 +9,7 @@ import 'package:your_comfort_food/page/home_page/widgets/recipe_all.dart';
 import 'package:your_comfort_food/page/home_page/widgets/recipe_dairy_free.dart';
 import 'package:your_comfort_food/page/home_page/widgets/recipe_vegan.dart';
 import 'package:your_comfort_food/page/home_page/widgets/search_container_widget.dart';
+import 'package:your_comfort_food/page/login_page/login_page.dart';
 import 'package:your_comfort_food/page/login_page/login_view_model.dart';
 import 'package:your_comfort_food/page/search_page/search_page.dart';
 import 'package:your_comfort_food/page/search_page/search_page_view_model.dart';
@@ -48,6 +49,60 @@ class _HomePageState extends State<HomePage> {
           style: TextStyleConstant.poppinsRegular
               .copyWith(fontWeight: FontWeight.bold, fontSize: 23),
         ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('Log out'),
+                    content: const Text('You sure want to log out?'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          providerLogin.logOut();
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginPage()),
+                              (route) => false);
+                        },
+                        child: const Text('Log Out'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Cancel'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Container(
+              margin: const EdgeInsets.only(right: 30),
+              width: 42,
+              height: 42,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: ColorConstant.whitishGray,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: const Offset(0, 0), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: const Icon(
+                Icons.logout_outlined,
+              ),
+            ),
+          )
+        ],
       ),
       body: Column(
         children: [
