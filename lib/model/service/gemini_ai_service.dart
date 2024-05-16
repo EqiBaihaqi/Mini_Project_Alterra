e971075956ad0ef6bd7b6dfc26789ee25c88baa4
EqiBaihaqi <baihaqi08.aa@gmail.com>
2024-05-16 14:08:32 +0700

    Change prompt and base url

--- lib/model/service/gemini_ai_service.dart
+++ lib/model/service/gemini_ai_service.dart
@@ -7,7 +7,7 @@ class GeminiService {
 
   static Future<String> getGeminiResponse(String? ingredient) async {
     final String question =
-        'Tolong berikan alternatif bahan masakan dari $ingredient, tolong berikan jawaban yang tidak bertele-tele dan langsung ke inti jawabannya dan jawab menggunakan bahasa inggris, tolong jangan gunakan ikon *. dan jika $ingredient bukanlah sebuah bahan masakan, maka jawab "maaf $ingredient bukanlah bahan masakan" dalam bahasa inggris ';
+        'Tolong berikan alternatif bahan masakan dari $ingredient, tolong berikan jawaban yang tidak bertele-tele dan langsung ke inti jawabannya dan jawab menggunakan bahasa inggris, tolong jangan gunakan ikon *. dan jika $ingredient bukanlah sebuah bahan masakan, maka jawab "maaf $ingredient bukanlah bahan masakan" dalam bahasa inggris, dan jika $ingredient adalah sebuah bahan masakan, maka berikan alternatif bahan masakan dari $ingredient ';
     Completer<String> completer = Completer<String>();
     gemini.streamGenerateContent(question).listen((event) {
       String response = event.content?.parts?.fold('',
